4a7b84883ec2e91c710e36c828a0e8886de5f3df
kamrulkoche <kamrulkoche@gmail.com>
2024-04-30 14:31:13 +0600

    latest add

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,18 @@ const loadDiscuss = async () => {
   );
   const data = await res.json();
   const discussData = data.posts;
-  console.log(discussData);
+  // console.log(discussData);
   displayDiscuss(discussData);
 };
 
 const displayDiscuss = (discussData) => {
   const discussContainer = document.getElementById("discuss-container");
-  console.log(discussData);
+  // console.log(discussData);
 
   discussData.forEach((discuss) => {
     // console.log(discuss);
     const discussCard = document.createElement("div");
-    
+
     // discussCard.classList=`grid gap-8 border bg-[#F2F2FF] p-6 rounded-3xl`;
     discussCard.innerHTML = `
         <div class="grid gap-8 border bg-[#F2F2FF] p-6 rounded-3xl">
@@ -68,20 +68,18 @@ const displayDiscuss = (discussData) => {
       </div>
         `;
     discussContainer.appendChild(discussCard);
-
-    
   });
 };
-let count = 0;
-function handleShowDetails(title="",view_count=""){
 
+let count = 0;
+function handleShowDetails(title = "", view_count = "") {
   count++;
   console.log(count);
-  const markCount=document.getElementById("mark-count");
-  markCount.innerText=count;
+  const markCount = document.getElementById("mark-count");
+  markCount.innerText = count;
   const discussTitle = document.getElementById("discuss-title");
   const titleField = document.createElement("div");
-  console.log("Hello",title,view_count);
+  console.log("Hello", title, view_count);
   titleField.innerHTML = `
       <div
       class="flex gap-10 items-center justify-between bg-white p-4 rounded-3xl"
@@ -99,4 +97,67 @@ function handleShowDetails(title="",view_count=""){
   discussTitle.appendChild(titleField);
 }
 
+const loadLatest = async () => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/retro-forum/latest-posts`
+  );
+  const data = await res.json();
+  const latestData = data;
+  //console.log(latestData);
+  displayLatest(latestData);
+};
+
+const displayLatest = (latestData) => {
+  const latestContainer = document.getElementById("latest-container");
+  console.log(latestData);
+
+  latestData.forEach((latest) => {
+    console.log(latest.author.posted_date ?? "N/A");
+    const latestCard = document.createElement("div");
+    latestCard.innerHTML = `
+    <div>
+    <div class="border-2 rounded-2xl p-5">
+      <div>
+      <img
+        src="${latest.cover_image}"
+        />
+      </div>
+
+      <div class="grid gap-1 mt-5">
+        <div class="flex items-center gap-4">
+          <i class="fa-solid fa-calendar"></i>
+          <p class="text-[#12132D] font-normal text-base">
+            ${latest.author.posted_date ?? "N/A"}
+          </p>
+        </div>
+        <h3 class="text-xl font-extrabold text-black">
+         ${latest.title}
+        </h3>
+        <p class="text-[#12132D] font-normal text-base">
+        ${latest.description}
+        </p>
+        <div class="flex gap-4 items-center">
+          <div class="avatar">
+            <div class="w-10 h-10 rounded-full">
+              <img
+                src="${latest.profile_image}"
+              />
+            </div>
+          </div>
+          <div>
+            <h3 class="text-lg font-bold text-black">
+            ${latest.author.name ?? "N/A"}
+            </h3>
+            <p class="text-[#12132D] font-normal text-base"> ${latest.author.designation ?? "N/A"}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+    latestContainer.appendChild(latestCard);
+  });
+};
+
 loadDiscuss();
+loadLatest();
